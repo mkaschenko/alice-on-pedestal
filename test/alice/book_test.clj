eff0d5f6eb9d5385307195ad4efb1825f19891dd
Maxim Kashchenko <mkaschenko@me.com>
2016-04-30 15:15:20 +0700

    Remove superfluous book tests

--- test/alice/book_test.clj
+++ test/alice/book_test.clj
@@ -4,16 +4,6 @@
 
 (deftest paginate-test
   (is (=
-       (book/paginate ["Alice" "Rabbit" "Cat" "Horse"] 2)
-       ["Alice\nRabbit" "Cat\nHorse"])))
-
-(deftest book-title-test
-  (is (= book/title "Alice's Adventures in Wonderland")))
-
-(deftest book-pages-test
-  (is (.startsWith (first book/pages) "CHAPTER I. Down the Rabbit-Hole"))
-  (is (.endsWith (last book/pages) "THE END"))
-  (is (= (count book/pages) 111)))
        (book/paginate ["Alice" "Caterpillar" "Cheshire Cat"
                        "Jabberwock" "White Rabbit"] 2)
        [["Alice" "Caterpillar"] ["Cheshire Cat" "Jabberwock"] ["White Rabbit"]])))
