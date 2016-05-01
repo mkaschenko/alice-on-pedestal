c2f400e857914cbf87a4d845183b075fc170d598
Maxim Kashchenko <mkaschenko@me.com>
2016-05-01 21:57:25 +0700

    Add back link and show current page number

--- src/alice/views.clj
+++ src/alice/views.clj
@@ -2,17 +2,26 @@
   (:require [clojure.string :as string]
             [io.pedestal.http.route :as route]))
 
+(defn back-link
+  [page-number min-page-number]
+  (if (< min-page-number page-number)
+    (format "<a href=\"%s\">back</a>"
+            (route/url-for :book-page :params {:id (dec page-number)}))
+    ""))
+
 (defn continue-link
-  [page-number last-page-number]
-  (if (< page-number last-page-number)
+  [page-number max-page-number]
+  (if (< page-number max-page-number)
     (format "<a href=\"%s\">continue</a>"
             (route/url-for :book-page :params {:id (inc page-number)}))
     ""))
 
 (defn book-page
   [{:keys [:title :pages :pages-count]} page-number]
-  (format "<title>%s</title><h1>%s</h1><div>%s</div></br><div>%s</div>"
+  (format "<title>%s</title><h1>%s</h1><div>%s -- %d -- %s</div></br><div>%s</div>"
           title
           title
+          (back-link page-number 1)
+          page-number
           (continue-link page-number pages-count)
           (string/join "</br>" (nth pages (dec page-number)))))
